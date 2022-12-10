316a6e099305740fc0556befe7c18904621f125c
Amadeo Valar <amadeo.valar@gmail.com>
2022-12-10 14:36:26 +0100

    Successful equilateral tests

--- src/test/java/at/aau/serg/exercises/shapetesting/RectangularShapeFactoryTest.java
+++ src/test/java/at/aau/serg/exercises/shapetesting/RectangularShapeFactoryTest.java
@@ -1,8 +1,29 @@
 // Amadeo Valar 11717299
 package at.aau.serg.exercises.shapetesting;
 
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
 import org.junit.jupiter.api.Test;
 
 public class RectangularShapeFactoryTest {
+    
+    RectangularShapeFactory shapeFactory = new RectangularShapeFactory();
+    
+    @Test
+    public void equilateralSquareTest() {
+        Square testSquare = (Square) shapeFactory.create(5);
+
+        assertTrue(testSquare.isEquilateral());
+
+    }
+    
+    public void equilateralRectangleTest() {
+        Rectangle testRectangle = (Rectangle) shapeFactory.create(5, 6);
+
+        assertFalse(testRectangle.isEquilateral());
+    }
+    
+    
     
 }
