c9ad0941042a1120471b74a99cd3812fe8ed099d
Amadeo Valar <amadeo.valar@gmail.com>
2022-12-10 23:32:32 +0100

    getSides tests working

---
+++ src/main/java/at/aau/serg/exercises/shapetesting/main.java
@@ -0,0 +1,22 @@
+package at.aau.serg.exercises.shapetesting;
+
+import java.util.Iterator;
+
+public class main {
+    public static void main(String[] args) {
+        int side = 10;
+        Square squareSides = new Square(side);
+        Rectangle rectangleSides = new Rectangle(side, side);
+        
+        System.out.println(rectangleSides.getSides());
+        System.out.println(squareSides.getSides().iterator());
+        
+        Iterator<Side> iter = squareSides.getSides().iterator();
+
+        while (iter.hasNext()) {
+            System.out.println("All the sides are " + iter.next().getLength());
+        }
+    
+    }
+
+}
--- src/test/java/at/aau/serg/exercises/shapetesting/RectangularShapeFactoryTest.java
+++ src/test/java/at/aau/serg/exercises/shapetesting/RectangularShapeFactoryTest.java
@@ -7,12 +7,27 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.util.Iterator;
+
 import org.junit.jupiter.api.Test;
 
 public class RectangularShapeFactoryTest {
     
+    // @BeforeAll
+    // public RectangularShape setUp(int width, int height) {
+    //     RectangularShapeFactory shapeFactory = new RectangularShapeFactory();
+    //     if (width == height) {
+    //         RectangularShape shape = shapeFactory.create(width);
+    //     } else {
+    //         RectangularShape shape = shapeFactory.create(width, height);
+    //     }
+        
+    //     return shape;
+    // }
     RectangularShapeFactory shapeFactory = new RectangularShapeFactory();
-    
+
+
+
     @Test
     // Equilateral Tests
     public void equilateralSquareTest() {
@@ -55,6 +70,40 @@ public class RectangularShapeFactoryTest {
         assertThrows(IllegalArgumentException.class, () -> shapeFactory.create(0,5));
     }
 
+    @Test
+    // 4 Side Objekte Test
+    public void getSidesTestSquare() {
+        int side = 5;
+        Square squareSides = new Square(side);
+
+        Iterator<Side> iter = squareSides.getSides().iterator();
+        
+        while (iter.hasNext()) {
+            assertEquals(side, iter.next().getLength());
+        }
+        // assertEquals(side, squareSides.getSides().get(0).getLength());
+        // assertEquals(side, squareSides.getSides().get(1).getLength());
+        // assertEquals(side, squareSides.getSides().get(2).getLength());
+        // assertEquals(side, squareSides.getSides().get(3).getLength());
+    }
+
+    @Test
+    public void getsidesTestRectangle() {
+        int width = 10;
+        int height = 10;
+
+        Rectangle rectangleSides = new Rectangle(width, height);
+        
+        for (int i = 0; i < rectangleSides.getSides().size(); i++) {
+            if(i % 2 == 0){
+                assertEquals(height, rectangleSides.getSides().get(i).getLength());
+            } else {
+                assertEquals(width, rectangleSides.getSides().get(i).getLength());
+            }
+            
+        }
+
+    }
 
     
     
