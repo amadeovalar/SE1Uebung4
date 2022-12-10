939bbb74dac1bfc114bf015dbbba59920000f51a
Amadeo Valar <amadeo.valar@gmail.com>
2022-12-10 14:50:56 +0100

    Equivalence tests working

--- src/test/java/at/aau/serg/exercises/shapetesting/RectangularShapeFactoryTest.java
+++ src/test/java/at/aau/serg/exercises/shapetesting/RectangularShapeFactoryTest.java
@@ -1,6 +1,8 @@
 // Amadeo Valar 11717299
 package at.aau.serg.exercises.shapetesting;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
@@ -11,6 +13,7 @@ public class RectangularShapeFactoryTest {
     RectangularShapeFactory shapeFactory = new RectangularShapeFactory();
     
     @Test
+    // Equilateral Tests
     public void equilateralSquareTest() {
         Square testSquare = (Square) shapeFactory.create(5);
 
@@ -18,11 +21,32 @@ public class RectangularShapeFactoryTest {
 
     }
     
+    @Test
     public void equilateralRectangleTest() {
         Rectangle testRectangle = (Rectangle) shapeFactory.create(5, 6);
 
         assertFalse(testRectangle.isEquilateral());
     }
+    
+    @Test
+    // Äquivalenz Tests
+    public void equivalenceTest() {
+        Square normalSquare = new Square(10);
+        Rectangle equalsideRectangle = new Rectangle(10, 10);
+
+        assertEquals(normalSquare, equalsideRectangle);
+    }
+    
+    @Test
+    public void equivalenceNotTest() {
+        Square secondNormalSquare = new Square(20);
+        Rectangle notEqualSideRectangle = new Rectangle(10, 15);
+
+        assertNotEquals(secondNormalSquare, notEqualSideRectangle);
+    }
+
+    
+    
     
     
     
