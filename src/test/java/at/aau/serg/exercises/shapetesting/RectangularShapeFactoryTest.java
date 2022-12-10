ab8b9da9c6873fee893ecb9815b43f4e1101ea40
Amadeo Valar <amadeo.valar@gmail.com>
2022-12-10 15:33:45 +0100

    Cleaning the branch - ready for merge

--- src/test/java/at/aau/serg/exercises/shapetesting/RectangularShapeFactoryTest.java
+++ src/test/java/at/aau/serg/exercises/shapetesting/RectangularShapeFactoryTest.java
@@ -8,7 +8,6 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
-import org.w3c.dom.css.Rect;
 
 public class RectangularShapeFactoryTest {
     
