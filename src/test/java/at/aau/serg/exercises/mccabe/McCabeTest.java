aa6124f0fd7cdfa66bcfa46dab0b4fa575c9b5f7
Amadeo Valar <amadeo.valar@gmail.com>
2022-12-12 20:54:17 +0100

    McCabe test done

--- src/test/java/at/aau/serg/exercises/mccabe/McCabeTest.java
+++ src/test/java/at/aau/serg/exercises/mccabe/McCabeTest.java
@@ -1,6 +1,38 @@
 package at.aau.serg.exercises.mccabe;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 
 public class McCabeTest {
+
+    @Test
+    public void pathOneTest() {
+        McCabe mcCabe = new McCabe();
+        int zahl1 = 5;
+        int zahl2 = 0;
+        int mcCabeResult = mcCabe.ggt(zahl1, zahl2);
+
+        assertEquals(zahl1, mcCabeResult);
+    }
+    @Test
+    public void pathTwoTest() {
+        McCabe mcCabe = new McCabe();
+        int zahl1 = 5;
+        int zahl2 = 4;
+        int mcCabeResult = mcCabe.ggt(zahl1, zahl2);
+
+        assertEquals(1, mcCabeResult);
+    }
+    @Test
+    public void pathThreeTest() {
+        McCabe mcCabe = new McCabe();
+        int zahl1 = 4;
+        int zahl2 = 5;
+        int mcCabeResult = mcCabe.ggt(zahl1, zahl2);
+
+        assertEquals(1, mcCabeResult);
+    }
+
 }
