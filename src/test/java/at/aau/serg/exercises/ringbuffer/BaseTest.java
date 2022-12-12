03fe3668e341a6d268e2fcbc3ff19a1ab7ec7832
Amadeo Valar <amadeo.valar@gmail.com>
2022-12-12 18:01:37 +0100

    Pit test, works also for Ringbuffer

--- src/test/java/at/aau/serg/exercises/ringbuffer/BaseTest.java
+++ src/test/java/at/aau/serg/exercises/ringbuffer/BaseTest.java
@@ -11,7 +11,9 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.NoSuchElementException;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+
+
 // import org.junit.platform.runner.JUnitPlatform;
 // import org.junit.runner.RunWith;
 
