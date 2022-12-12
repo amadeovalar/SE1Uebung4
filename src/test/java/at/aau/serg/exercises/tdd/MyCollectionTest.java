5453cec350c807f3dfe241cc839a10eb1369c25e
Amadeo Valar <amadeo.valar@gmail.com>
2022-12-12 21:16:02 +0100

    Testing the change in size for the remove method

--- src/test/java/at/aau/serg/exercises/tdd/MyCollectionTest.java
+++ src/test/java/at/aau/serg/exercises/tdd/MyCollectionTest.java
@@ -9,5 +9,20 @@ import static org.junit.Assert.assertEquals;
 
 public class MyCollectionTest
 {
+    @Test
+    public void testArraySizeAfterRemove() {
+        int capacity = 2;
+        MyCollection myCollection = new MyCollection(capacity);
 
+        String firstElement = "First element";
+        String secondElement = "Second element";
+
+        myCollection.add(firstElement);
+        myCollection.add(secondElement);
+
+
+        myCollection.remove(firstElement);
+
+        assertEquals(capacity - 1, myCollection.size());
+    }
 }
